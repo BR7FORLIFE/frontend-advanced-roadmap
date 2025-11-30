6d831c8df8dfcf6fabe1f94a73de770f90587a3b
br7forlife <diazpachecobrayan@gmail.com>
2025-11-30 12:45:03 -0500

    defined some routes

--- AngularFramework/Life-Cycles-Hooks/src/app/app.routes.ts
+++ AngularFramework/Life-Cycles-Hooks/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from '@features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard,
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then((routes) => routes.DASHBOARD_ROUTES),
+  },
+];
