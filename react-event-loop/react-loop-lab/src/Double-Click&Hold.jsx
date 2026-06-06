45dac5f81c4f17c993a0a58d2f82f4b9d2c2fac7
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-06-06 07:39:26 -0500

    feat: started Live Product Project with react native

---
+++ react-event-loop/react-loop-lab/src/Double-Click&Hold.jsx
@@ -0,0 +1,13 @@
+import {useBurstTrigger} from './hooks/useBurstTrigger'
+
+const delayTimer = 1000 //esperamos un segundo si quiere hacer otro click (doble click)
+
+export function DoubleClickAndHold(){
+
+    //const {} = useBurstTrigger(()=> {}, delayTimer)
+
+
+    return (
+        <></>
+    )
+}
---
+++ react-event-loop/react-loop-lab/src/hooks/useBurstTrigger.jsx
@@ -0,0 +1,4 @@
+export function useBurstTrigger(callback, delay){
+
+    
+}
